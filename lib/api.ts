1d247e3488c1d58c475619c7768c9f4bcfb890b5
Mostafa <kahanimostafa83@gmail.com>
2025-02-28 01:01:11 +0330

    Initial commit from Create Next App

---
+++ lib/api.ts
@@ -0,0 +1,18 @@
+export interface Post {
+  id: number;
+  userId: number;
+  title: string;
+  body: string;
+}
+
+export interface User {
+  id: number;
+  name: string;
+  username: string;
+  email: string;
+  website: string;
+}
+
+export interface PostWithAuthor extends Post {
+  author: string;
+}
